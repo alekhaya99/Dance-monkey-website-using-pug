e43bcf169a51abe4dad923867d4f5d3a124fea33
alekhaya99 <alekhaya.datta@gmail.com>
2020-12-13 21:47:40 +0800

    udpdate

---
+++ app.js
@@ -0,0 +1,25 @@
+const express=require("express");
+const path=require("path");
+const fs = require("fs");
+const app=express();
+const port=8000;
+
+//EXPRESS
+
+app.use('./static',express.static('static'));
+app.use(express.urlencoded());
+app.use('/static', express.static('static'));
+
+//PUG
+app.set('view engine','pug');
+app.set('views',path.join(__dirname,'views'));
+
+app.get("/",(req,res)=>{
+    
+    const parameters= {};
+    res.status(200).render('index.pug',parameters)
+});
+
+app.listen(port,()=>{
+    console.log(`Listening on port ${port}`)
+})
